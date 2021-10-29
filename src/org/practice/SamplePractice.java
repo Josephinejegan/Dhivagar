a39986b48fbbf4046035b60eb2a84cedde8d162e
Josephinejegan <josephinejegan5@gmail.com>
2021-10-29 05:51:31 -0700

    Dhiva

---
+++ src/org/practice/SamplePractice.java
@@ -0,0 +1,17 @@
+package org.practice;
+
+public class SamplePractice {
+	public static void main(String[] args) {
+	//ASCII
+//		char s = 'y';
+//		int v = s;
+//		System.out.println(v);
+	//converting string into integer
+		String s1 ="53672";
+		int i = Integer.parseInt(s1);
+		System.out.println(i);
+		
+	}
+	}
+
+
